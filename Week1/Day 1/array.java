ddd4b20a5193dc252a7295465cfac1f48e6cb594
Guru-1110 <prasathguru1011@gmail.com>
2026-08-15 19:45:40 +0530

    First code

---
+++ Week1/Day 1/array.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class array{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int C = sc.nextInt();
+        int A = sc.nextInt();
+        System.out.println(N);
+        System.out.println(C);
+        System.out.println(A);
+
+
+    }
+}
