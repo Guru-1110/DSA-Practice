0dd3710d3176ecac7db1a7388ca8a181dc5aa629
Guru-1110 <prasathguru1011@gmail.com>
2026-09-05 14:14:23 +0530

    still Going

--- Week-3/Day-14/Error.java
+++ Week-3/Day-14/Error.java
@@ -1,9 +1,24 @@
 public class Error{
     public static void main(String[] args){
-        int i = 0;
+        int i = 2;
         int j = 0;
+        int arr[] = new int[5];
+        String str = "Guru";
         try{
             j = 20/i;
+            System.out.println(str.length());
+            System.out.println(arr[1]);
+            System.out.println(arr[5]);
+        }
+        catch(ArithmeticException e){
+            System.out.println("Cant divide by zero");
+        }
+        catch(ArrayIndexOutOfBoundsException e){
+            System.out.println("Know your limits");
+        }
+
+        catch(NullPointerException e){
+            System.out.println("Null pointer exception");
         }
         catch(Exception e){
             System.out.println("Something went wrong");
