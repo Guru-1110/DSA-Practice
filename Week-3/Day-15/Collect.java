31bd45be80e6d14d487418f0c63b263655c72317
Guru-1110 <prasathguru1011@gmail.com>
2026-09-06 10:40:05 +0530

    Collection

---
+++ Week-3/Day-15/Collect.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+import java.util.List;
+public class Collect{
+    public static void main(String[] args){
+        List <Integer> c = new ArrayList<Integer>();
+        c.add(10);
+        c.add(4);
+        c.add(0);
+        c.add(1);
+        c.add(7);
+        System.out.println(c.get(2));
+
+    }
+}
