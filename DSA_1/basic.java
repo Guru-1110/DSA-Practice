6741fda1860dde30784fc9f24f9baa2498449913
Guru-1110 <prasathguru1011@gmail.com>
2026-09-07 22:58:58 +0530

    Start Date of dsa 7/9/26

---
+++ DSA_1/basic.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+public class basic{
+    public static void main(String[] args){
+        Scanner s = new Scanner(System.in);
+        int n =s.nextInt();
+        System.out.print(n);
+    }
+}
