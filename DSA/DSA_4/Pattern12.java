581773bf62b7b5cdd355506adbf86d7458302ba2
Guru-1110 <prasathguru1011@gmail.com>
2026-09-12 17:35:03 +0530

    I will complete pattern today

---
+++ DSA/DSA_4/Pattern12.java
@@ -0,0 +1,19 @@
+public class Pattern12{
+    public static void main(String[] args) {
+        int n = 4;
+        for(int i = 1; i<=n; i++){
+            for(int j = 1; j<=i; j++){
+                System.out.print(j);
+            }
+            for(int k =1 ;k<2*(n-i)+1;k++){
+                System.out.print(" ");
+
+            }
+            for(int l = i; l>0; l--){
+                System.out.print(l);
+                
+            }
+            System.out.println();
+        }
+    }
+}
